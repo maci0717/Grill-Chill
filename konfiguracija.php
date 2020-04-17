879dd7ae6d28aeaea2e5458e0c6c72578fdba3da
maci0717 <57645937+maci0717@users.noreply.github.com>
2020-04-17 21:29:12 +0200

    Promjenjeni podaci sa lokanog na web

--- konfiguracija.php
+++ konfiguracija.php
@@ -4,19 +4,20 @@ return [
     'nazivAPP' => 'Grill & Chill',
     'url' => 'http://maciserver01.hr/',
     'dev' => true,
+    /*
     'db' =>[
         'server' => 'localhost',
         'baza' => 'restoran',
         'korisnik' => 'edunova',
         'lozinka' => 'edunova'
-    ]
-    /*
+    ]*/
+   
     'db' =>[
         'server' => 'localhost',
         'baza' => 'ereb_restoran',
         'korisnik' => 'ereb_ereb',
         'lozinka' => 'J-3I59KW-{r*'
-    ]*/
+    ]
     
 ];
 
